dabbd3babed2901e0ee1dd6761b828dc24bf86ff
Firas Ghedir <firas.ghedir@uni-ulm.de>
2018-10-28 23:29:57 +0100

    Update Masterprojekt/src/Main.java

--- Masterprojekt/src/Main.java
+++ Masterprojekt/src/Main.java
@@ -1,10 +1,18 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+import GraphGenerators.Graph;
 import GraphGenerators.GridGraphGenerator;
 
 public class Main {
 	
 	public static <V, E> void main(String[] args) {
 		
+		GridGraphGenerator<V,E> test = new GridGraphGenerator<>(5,5);
+		Map<String,V> map= new TreeMap<>();
+		Graph<V,E> graph = null;
 		
+		test.generateGraph(graph,map);
 
 	}
 
