c8c47f5c33158d49c8071e0d1e24c5f532f09802
Julian Bestler <julian.bestler@uni-ulm.de>
2019-03-28 16:54:06 +0100

    actualisation of Nickerl.java

--- Masterprojekt/src/nickerl/Nickerl.java
+++ Masterprojekt/src/nickerl/Nickerl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+import genetic.heuristic.GaMINTB;
 import graphGenerator.GridGraphGenerator;
 import graphModel.Edge;
 import graphModel.Graphs;
@@ -26,6 +27,10 @@ public class Nickerl {
 	ArrayList<Integer> y;
 	ArrayList<Player> players;
 
+	static double delta;
+
+	boolean debug = false;
+
 	public Nickerl(Graphs g) throws IloException {
 		this.g = g;
 		cplex = new IloCplex();
@@ -34,12 +39,19 @@ public class Nickerl {
 	}
 
 	private Graphs newgraph(Graphs g2, int x) {
+
+		if (debug) {
+			System.out.println("Start of newgraph");
+		}
+
 		Graphs graph = new Graphs();
 		graph.setVertices(g2.getVertices());
-		for (int i = 0; i < g2.getEdges().size(); i++) {
+		for (int i = 0; i < g2.getEdges().size(); i++) { // go through all edges of the original graph
 
-			if (g2.getEdges().get(i).getValues().get(x) > 0) {
-				graph.getEdges().add(g2.getEdges().get(i));
+			if (g2.getEdges().get(i).getValues().get(x) > 0) { // get the flow in the SO of player x on edge i
+				graph.getEdges().add(g2.getEdges().get(i)); // add only if the flow is non-zero
+
+				// TODO: Check if this really is proper call by value and nothing weird happens
 			}
 
 		}
@@ -47,30 +59,108 @@ public class Nickerl {
 		return graph;
 	}
 
+	private void printGraph(Graphs toPrint) {
+		for (int i = 0; i < toPrint.getAdjacencyMatrix().length; i++) {
+			for (int j = 0; j < toPrint.getAdjacencyMatrix().length; j++) {
+				System.out.print(toPrint.getAdjacencyMatrix()[i][j]);
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+
+	private void printShortestPath(List<Edge> KP) {
+		for (Edge i : KP) {
+			System.out.println("(" + i.getFrom().getId() + "," + i.getTo().getId() + ")");
+		}
+	}
+
+	private void printNonzeroFlows(Graphs toPrint, int p) {
+		for (Edge i : toPrint.getEdges()) {
+			if (i.getValues().get(p) != 0) {
+				System.out.println("(" + i.getFrom().getId() + "," + i.getTo().getId() + ") : " + i.getValues().get(p));
+			}
+		}
+	}
+
 	public void step1() throws IloException {
+
+		// g is the graph
+		if (debug) {
+			System.out.println("Start of step 1");
+
+			System.out.println("Original Graph:");
+			printGraph(g);
+		}
+
 		players = new ArrayList<>(g.getPlayers());
-		for (int i = 0; i < g.getPlayers().size(); i++) {
-			while (g.getPlayers().get(i).getDemand() > 0) {
-				Graphs x = newgraph(g, i);
+		for (int i = 0; i < g.getPlayers().size(); i++) { // go through all the players
+
+			if (debug) {
+				System.out.println("Data of player " + i);
+				System.out.println("Source: " + g.getPlayers().get(i).getSource().getId());
+				System.out.println("Sink: " + g.getPlayers().get(i).getSink().getId());
+				System.out.println("Demand: " + g.getPlayers().get(i).getDemand());
+			}
+
+			while (g.getPlayers().get(i).getDemand() > 0) { // If the demand of the current player is not 0
+				Graphs x = newgraph(g, i); // A new graph is generated
+
+				if (debug) {
+					System.out.println("Graph for player " + i);
+					printGraph(x);
+				}
+
 				List<Edge> KP = BellmanFordShortestPath
 						.findPathBetween(x, g.getPlayers().get(i).getSource(), g.getPlayers().get(i).getSink())
-						.getEdgeList();
+						.getEdgeList(); // This generates the edges on a non-zero flow shortest path
+
+				if (debug) {
+					System.out.println("Current shortest Path");
+					printShortestPath(KP);
+
+					printNonzeroFlows(x, i);
+				}
 
 				ArrayList<Double> tmp = new ArrayList<>();
 
 				for (int k = 0; k < KP.size(); k++) {
-					tmp.add(KP.get(k).getValues().get(i));
+					tmp.add(KP.get(k).getValues().get(i)); // save the value of the SO for each edge on the shortest
+															// path
+															// for player i
 				}
 
-				double fmax = Collections.min(tmp);
-				Player sigma = new Player(players.size(), g.getPlayers().get(i).getSource(),
-						g.getPlayers().get(i).getSink(), fmax);
-				players.add(sigma);
+				double fmax = Collections.min(tmp); // How much flow goes over this path?
 
-				for (int k = 0; k < g.getEdges().size(); k++) {
-					if (KP.contains(g.getEdges().get(k))) {
+				if (debug) {
+					System.out.println("Current fmax: " + fmax);
+				}
+
+				// TODO: add this code to final
+				// maybe we have to break out earlier...
+				if (g.getPlayers().get(i).getDemand() - fmax < getDelta()) {
+					tmp.clear();
+					break;
+				}
+
+				Player sigma = new Player(players.size(), g.getPlayers().get(i).getSource(),
+						g.getPlayers().get(i).getSink(), fmax); // create a new commodity.
+																// Source and sink come from i.
+																// demand is the flow going over the path
+				players.add(sigma);
+				// TODO: Not sure if something janky is going on here... How do the IDs work?? I
+				// hope they start at 0...
+
+				// Update the SO flows and stuff in g
+				for (int k = 0; k < g.getEdges().size(); k++) { // go through all the edges of the graph
+					if (KP.contains(g.getEdges().get(k))) { // if the edge is in the shortest path...
 						g.getEdges().get(k).getValues().set(g.getPlayers().get(i).getId(),
 								g.getEdges().get(k).getValues().get(g.getPlayers().get(i).getId()) - fmax);
+
+						// TODO: This sets values below 0.0001 to 0. Update that everywhere necessary
+						if (g.getEdges().get(k).getValues().get(g.getPlayers().get(i).getId()) < getDelta()) {
+							g.getEdges().get(k).getValues().set(g.getPlayers().get(i).getId(), 0.0);
+						}
 						g.getEdges().get(k).getValues().add(sigma.getId(), fmax);
 
 					} else {
@@ -85,140 +175,267 @@ public class Nickerl {
 			}
 
 		}
-	
+
+		if (debug) {
+			System.out.println("Same original graph?");
+			printGraph(g);
+		}
+
 		step2(0);
 	}
 
 	// sublisthead is the id where the sublist begins
 	public void step2(int sublisthead) throws IloException {
 
+		if (debug) {
+			System.out.println("Start of step 2");
+		}
+
+		// TODO: are there IDs getting shuffeld too that maybe shouldn't?
 		Collections.shuffle(g.getEdges().subList(sublisthead, g.getEdges().size()));
 		// Collections.shuffle(g.getEdges());
 		for (int i = 0; i < g.getEdges().size(); i++) {
-			g.getEdges().get(i).setBetta(0.0);
+			g.getEdges().get(i).setBetta(0.0); // Initialize all Beta = 0
 
 		}
-		System.out.println("I am jumping to step 3");
 
 		step3();
+
+		if (debug) {
+			System.out.println("End of step 2");
+		}
 	}
 
 	public boolean step3() throws IloException {
-		ArrayList<Player> p0 = new ArrayList<>(players);
-		ArrayList<ArrayList<Player>> list = new ArrayList<>();
-		list.add(p0);
-		for (int i = 1; i < g.getEdges().size(); i++) {
-			list.add(i, new ArrayList<Player>());
+
+		if (debug) {
+			System.out.println("Start of step 3");
 		}
-		for (int i = 1; i < g.getEdges().size(); i++) {
 
-			list.set(i, list.get(i - 1));
+		ArrayList<Player> p0 = new ArrayList<>(players); // p0 are just a copy of all players
+		ArrayList<ArrayList<Player>> list = new ArrayList<>(); // list remembers all past playerlists
 
-			System.out.println("before the while");
+		for (int i = 0; i < g.getEdges().size() - 1; i++) {
+			list.add(i, new ArrayList<Player>());
+		} // add an empty arraylist for each edge
+
+		// TODO: "i" can not necessarily be use equally as index for the list and as
+		// edge index!
+		// Check for everthing following whether "i" is used correcty or should be e.g.
+		// i-1
+
+		for (int id = 0; id < g.getEdges().size() - 1; id++) { // Go through all but the last edge as an id for the
+																// considered player list
+
+			// Are here call by reference problems?
+			if (id == 0) {
+				list.set(id, p0);
+			} else {
+				list.set(id, list.get(id - 1));
+			}
 
 			Boolean flag = true;
 			while (flag) {
 				int tmp = 0;
 				flag = false;
 
-				for (int j = 0; j < list.get(i).size(); j++) {
-					if (g.getEdges().get(i).getValues().get(list.get(i).get(j).getId()) > 0) {
+				// Finds a player and saves him in tmp that has non-zero flow on i
+				for (int j = 0; j < list.get(id).size(); j++) { // go through the player list
+
+					ArrayList<Edge> a = g.getEdges();
+					Edge b = a.get(id);
+					ArrayList<Double> c = b.getValues();
+					ArrayList<Player> d = list.get(id);
+					Player e = d.get(j);
+
+					if (debug) {
+						System.out.println("Is the node id the same as the index? " + (j == e.getId()));
+
+						System.out.println("Size of the current player List: " + list.get(id).size());
+						for (Player p : list.get(id)) {
+							System.out.println("ID of player: " + p.getId());
+						}
+
+					}
+
+					// Error occurs here. Maybe the edges don't get updated properly when a new
+					// commodity is created.
+					Double f = c.get(e.getId());
+
+					// TODO: A indexoutofboundsexpection occurs here. Reason seems to be, that the
+					// list of values of some edges don't get properly updated.
+
+					if (g.getEdges().get(id).getValues().get(list.get(id).get(j).getId()) > 0) { // i is the id for the
+																									// current player
+																									// list
 						tmp = j;
 						flag = true;
-						check(list.get(i), i, tmp);
+						check(list.get(id), id, tmp); // TODO: is the second i edge id or player list if?
 						break;
 					}
 				}
-
 			}
 		}
 
-		System.out.println("after the while");
-
-		for (int i = g.getEdges().size(); i > 1; i++) {
-			if (y.get(i) != 1) {
-				y.set(i, 0);
+		// TODO: This HAS!!! to be i--!! several things wrong here!!
+		if (debug) {
+			System.out.println("Do we even get here?");
+		}
+		for (int id = g.getEdges().size() - 1; id >= 0; id--) {
+			if (debug) {
+				System.out.println("The current size of the y vector: " + y.size());
 			}
-			g.setPlayer(list.get(i - 1));
+
+			if (y.get(id) != 1) {
+				y.set(id, 0);
+			}
+			if (id == 0) {
+				g.setPlayer(p0);
+			} else {
+				g.setPlayer(list.get(id - 1));
+			}
+
 			if (evaluation(g)) {
 				return true;
 			} else {
-				y.set(i - 1, 1);
+				y.set(id, 1);
 				if (evaluation(g)) {
 					return true;
 				} else {
-					y.set(i, 0);
-					step2(i);
+
+					if (debug) {
+						System.out.println("Even setting stuff to 1 led to infeasibility");
+					}
+					y.set(id, 0);
+					step2(id);
 
 				}
 			}
 
 		}
-		System.out.println("finish");
-
+		if (debug) {
+			System.out.println("End of step 3");
+		}
 		return false;
 	}
 
+	// Player tmp has non-zero flow
+	// alpha is the current player list p_id
+	// id is the current index
 	public void check(ArrayList<Player> alpha, int id, int tmp) {
 
+		if (debug) {
+			System.out.println("Start of check");
+
+			System.out.println("is id the same as index?" + " " + (tmp == alpha.get(tmp).getId()));
+		}
 		Player k1 = new Player(alpha.get(tmp).getId(), alpha.get(tmp).getSource(), g.getEdges().get(id).getFrom(),
 				alpha.get(tmp).getDemand());
-		Player k2 = new Player(alpha.size() , g.getEdges().get(id).getTo(), alpha.get(tmp).getSink(),
+		Player k2 = new Player(alpha.size(), g.getEdges().get(id).getTo(), alpha.get(tmp).getSink(),
 				alpha.get(tmp).getDemand());
 		g.getEdges().get(id).getValues().set(alpha.get(tmp).getId(), 0.0);
 		Graphs x = newgraph(g, alpha.get(tmp).getId());
 
-
-		
-		for (int i = 0; i < x.getAdjacencyMatrix().length; i++) {
-			for (int j = 0; j < x.getAdjacencyMatrix().length; j++) {
-				System.out.print(x.getAdjacencyMatrix()[i][j]);
-				System.out.print(" ");
+		if (debug) {
+			for (int i = 0; i < x.getAdjacencyMatrix().length; i++) {
+				for (int j = 0; j < x.getAdjacencyMatrix().length; j++) {
+					System.out.print(x.getAdjacencyMatrix()[i][j]);
+					System.out.print(" ");
+				}
+				System.out.println();
 			}
-			System.out.println();
 		}
 		GraphPath<Vertex, Edge> tmp2 = BellmanFordShortestPath.findPathBetween(x, k2.getSource(), k2.getSink());
-		
-		System.out.println(k1.getSource().getId() + " " + k1.getSink().getId());
-		System.out.println(k2.getSource().getId() + " " + k2.getSink().getId());
-		for(int i = 0 ; i < x.getEdges().size() ; i++) {
-			System.out.println("fluss im sozialen Optimum" + k1.getId() + " " +  x.getEdges().get(i).getValues().get(k1.getId()));
-			//System.out.println("fluss im sozialen Optimum" + k2.getId() + " " +  x.getEdges().get(i).getValues().get(k2.getId()));
+
+		if (debug) {
+			System.out.println(k1.getSource().getId() + " " + k1.getSink().getId());
+			System.out.println(k2.getSource().getId() + " " + k2.getSink().getId());
+
+			// TODO: A indexoutofboundexception occurs here somewhere
+			// This can't work!! Some of the edges were deleted in the making of x! (fixed)
+
+			ArrayList<Edge> a = g.getEdges();
+			Edge b = a.get(id); // TODO: Error occurs HERE!! Edge Ids aren't properly used it seems. (fixed)
+			ArrayList<Double> c = b.getValues();
+			Double d = c.get(k1.getId());
+
+			// TODO: Lists of values in edges for the new player k2 have not been updated
+			// yet!!
+
+			for (int i = 0; i < x.getEdges().size(); i++) {
+				System.out.println("fluss im sozialen Optimum " + k1.getId() + " "
+						+ g.getEdges().get(id).getValues().get(k1.getId()));
+				// System.out.println("fluss im sozialen Optimum " + k2.getId() + " " +
+				// g.getEdges().get(i).getValues().get(k2.getId()));
+
+			}
+
+			System.out.println("Is the shortest Path tmp2 null? " + (tmp2 == null));
+			if (tmp2 == null) {
+				System.out.println("s2 - t2: (" + k2.getSource().getId() + "," + k2.getSink().getId() + ")");
+			}
+		}
+
+		List<Edge> K2 = tmp2.getEdgeList();
+
+		if (debug) {
+			System.out.println(K2.size());
+		}
+
+		GraphPath<Vertex, Edge> tmp3 = BellmanFordShortestPath.findPathBetween(x, k1.getSource(), k1.getSink());
+
+		if (debug) {
+			System.out.println("Is the shortest Path tmp3 null? " + (tmp3 == null));
+			if (tmp3 == null) {
+				System.out.println("s1 - t1: (" + k1.getSource().getId() + "," + k1.getSink().getId() + ")");
+			}
+		}
+
+		List<Edge> K1 = tmp3.getEdgeList();
+
+		for (Edge e : g.getEdges()) {
+
+			e.getValues().add(k2.getId(), 0.0);
 
 		}
-		
-		
-		System.out.println(tmp2 == null);
-		if(tmp2 == null) {
-			System.out.println(k2.getSource().getId() == k2.getSink().getId());
-		}
-		List<Edge> K2 = tmp2.getEdgeList();
-		System.err.println(K2.size());
-		List<Edge> K1 = BellmanFordShortestPath.findPathBetween(x, k1.getSource(), k1.getSink()).getEdgeList();
-		
+
 		for (int i = 0; i < K1.size(); i++) {
-			K1.get(i).getValues().add(k1.getId(), K1.get(i).getValues().get(alpha.get(tmp).getId()));
+			K1.get(i).getValues().set(k1.getId(), K1.get(i).getValues().get(alpha.get(tmp).getId()));
 			K1.get(i).getValues().add(k2.getId(), 0.0);
-		//	K1.get(i).getValues().set(alpha.get(tmp).getId(), 0.0);
+			// K1.get(i).getValues().set(alpha.get(tmp).getId(), 0.0);
 
 		}
 
 		for (int i = 0; i < K2.size(); i++) {
-			K2.get(i).getValues().add(k1.getId(), 0.0);
+			K2.get(i).getValues().set(k1.getId(), 0.0);
 			K2.get(i).getValues().add(k2.getId(), K2.get(i).getValues().get(alpha.get(tmp).getId()));
-			//K2.get(i).getValues().set(alpha.get(tmp).getId(), 0.0);
+			// K2.get(i).getValues().set(alpha.get(tmp).getId(), 0.0);
 		}
 
-		alpha.remove(alpha.get(tmp));
+		// alpha.remove(alpha.get(tmp));
 
-		alpha.add(k1.getId(),k1);
-		alpha.add(k2.getId(),k2);
+		alpha.set(k1.getId(), k1);
+		alpha.add(k2.getId(), k2);
 
+		if (debug) {
+			System.out.println("End of check");
+		}
 	}
 
 	public void run() throws IloException {
 
+		if (debug) {
+			System.out.println("Start of run");
+		}
+		for (int i = 0; i < g.getEdges().size(); i++) {
+			y.add(0);
+		}
+
 		step1();
+
+		if (debug) {
+			System.out.println("End of run");
+		}
 		for (int i = 0; i < y.size(); i++) {
 			System.out.println(y.get(i));
 		}
@@ -306,7 +523,18 @@ public class Nickerl {
 		}
 	}
 
+	public double getDelta() {
+		return Nickerl.delta;
+	}
+
+	public static void setDelta(double delta) {
+		Nickerl.delta = delta;
+	}
+
 	public static void main(String[] args) throws IloException {
+
+		setDelta(0.001);
+
 		Map<String, Vertex> map = new TreeMap<>();
 		Graphs graph = new Graphs();
 		GridGraphGenerator test = new GridGraphGenerator(2, 2); // do not change !!
@@ -327,5 +555,4 @@ public class Nickerl {
 		social.solveDSSP(graph);
 		n.run();
 	}
-
 }
