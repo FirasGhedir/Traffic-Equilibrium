18698d420e66386fdd8ef6536b841ea65ce697dd
Julian Bestler <julian.bestler@uni-ulm.de>
2019-05-17 22:10:12 +0200

    MATLAB script expanded; plots added

---
+++ MATLAB/makeVectorsHaveSameDimension.m
@@ -0,0 +1,22 @@
+function [outputVector1,outputVector2] = makeVectorsHaveSameDimension(inputVector1,inputVector2)
+%This function handles two vectors so that they all have the 
+%   same dimension
+
+size1 = length(inputVector1);
+size2 = length(inputVector2);
+
+if size1 < size2
+            tmp1 = zeros(1,(size2-size1));
+            tmp2 = transpose(inputVector1);
+            outputVector1 = [tmp1 tmp2];
+            outputVector2 = inputVector2;
+end
+if size2 < size1
+            tmp1 = zeros(1,(size1-size2));
+            tmp2 = transpose(inputVector2);
+            outputVector1 = inputVector1;
+            outputVector2 = [tmp1 tmp2];
+end
+
+end
+
